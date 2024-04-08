594c910227910a49be86ddc44f3b6660e5b31208
ATHPIL <atat79353@gmail.com>
2024-04-09 02:32:23 +0530

    first commit

--- utils/features.js
+++ utils/features.js
@@ -1,9 +1,1 @@
-import jwt from "jsonwebtoken";
-
-
-export const sendCookie = (user , res)=>{ 
-const token=jwt.sign({
-    _id:user._id
-  },process.env.SECRET)
-  res.cookie("sumit",token).redirect("/")
-}
+c
